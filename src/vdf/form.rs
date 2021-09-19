cd03cf26a85a2d5e58aeef4bde5ecbce9e7e2efc
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-09-19 20:03:34 +0200

    Add generator method for QForm
    This uses Chia's technique from https://medium.com/@chia.net/chia-vdf-competition-guide-5382e1f4bd39

--- src/vdf/form.rs
+++ src/vdf/form.rs
@@ -1,4 +1,5 @@
-use num_bigint::BigUint;
+use num_bigint::BigInt;
+use num_traits::sign::Signed;
 
 /// This represents a binary quadratic form.
 ///
@@ -11,7 +12,22 @@ use num_bigint::BigUint;
 /// Operations assume that the forms they're operating on have the same discriminant.
 #[derive(Clone, Debug)]
 struct QForm {
-    a: BigUint,
-    b: BigUint,
-    c: BigUint,
+    a: BigInt,
+    b: BigInt,
+    c: BigInt,
+}
+
+impl QForm {
+    /// This creates the generator of a class group, with a given discriminant.
+    ///
+    /// The discriminant is expected to be the negation of a prime = 7 mod 8.
+    pub fn generator(discriminant: &BigInt) -> QForm {
+        debug_assert!(discriminant.is_negative(), "discriminator should be negative");
+        // The absolute value is 7 mod 8, but discriminator is negative, hence 1 mod 8.
+        debug_assert!(discriminant % 8 == 1.into(), "discriminator should be 7 mod 8");
+        let a = BigInt::from(2u64);
+        let b = BigInt::from(1u32);
+        let c = -(discriminant - 1u64) / (&a << 2);
+        return QForm { a, b, c };
+    }
 }
